d63455ef104a58fbd011cea7367f603840557333
Darshan Sonar <codecrackup@gmail.com>
2025-12-23 20:56:05 +0530

    Update web.py

--- web.py
+++ web.py
@@ -188,9 +188,14 @@ def delete(sno):
         db.session.commit()   
     return redirect('/admin')
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
 
 
  
+
